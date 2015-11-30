163c5a5def2b3e7ce4f081473b7bf89c4e2a913e
Chen Osipov <obender@OBenders-MacBook-Air.local>
2015-11-30 08:28:35 +0200

    Lates progression

--- static/javascripts/app.js
+++ static/javascripts/app.js
@@ -7,10 +7,10 @@ function drawTable() {
     data.addColumn('number', 'Salary');
     data.addColumn('boolean', 'Full Time Employee');
     data.addRows([
-        ['Mike',  {v: 10000, f: '$10,000'}, true],
-        ['Jim',   {v:8000,   f: '$8,000'},  false],
+        ['Mike', {v: 10000, f: '$10,000'}, true],
+        ['Jim', {v: 8000, f: '$8,000'}, false],
         ['Alice', {v: 12500, f: '$12,500'}, true],
-        ['Bob',   {v: 7000,  f: '$7,000'},  true]
+        ['Bob', {v: 7000, f: '$7,000'}, true]
     ]);
 
     var table = new google.visualization.Table(document.getElementById('table_div'));
@@ -22,19 +22,37 @@ function drawTable() {
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
         ['Year', 'Sales', 'Expenses'],
-        ['2004',  1000,      400],
-        ['2005',  1170,      460],
-        ['2006',  660,       1120],
-        ['2007',  1030,      540]
+        ['2004', 1000, 400],
+        ['2005', 1170, 460],
+        ['2006', 660, 1120],
+        ['2007', 1030, 540]
     ]);
 
     var options = {
         title: 'Company Performance',
         curveType: 'function',
-        legend: { position: 'bottom' }
+        legend: {position: 'bottom'}
     };
 
     var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
 
     chart.draw(data, options);
 }
+
+$.ajax({
+    url: "/data/sample_data.json"
+}).done(function (data) {
+    var resultsSet = Promise.filter(
+        data,
+        function (item, index, length) {
+            console.log(item);
+        }
+    ).done(function (data) {
+            alert("second success");
+        });
+    resultsSet.resolve();
+});
+
+
+
+
